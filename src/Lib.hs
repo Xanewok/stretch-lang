7063a49040b06739ddc0e6ef28a3a6c0eb323fd1
Igor Matuszewski <Xanewok@gmail.com>
2018-05-13 21:39:14 +0200

    Implement basic framework for the interpreter
    This evaluates parsed statements. Needs a State monad so the
    computations can affect each other. Basic expression evaluation is
    implemented.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,10 +2,78 @@ module Lib
     ( interpret
     ) where
 
-import BNFC.AbsStretch(Program)
+import Data.Map (Map)
+import Data.Typeable(Typeable)
+import qualified Data.Map as Map
+
+import BNFC.AbsStretch
+import BNFC.PrintStretch
 
 interpret :: Program -> IO ()
-interpret _tree = putStrLn "Real, hard work."
+interpret (ProgramEntry stmts) =
+    mapM_ (`evalStmt` initialState) stmts
+
+
+toBool :: Boolean -> Bool
+toBool Boolean_true = True
+toBool Boolean_false = False
+
+litToValue :: Literal -> Value
+litToValue lit = case lit of
+    LiteralUnit -> Unit ()
+    LiteralBoolean bool -> Boolean (toBool bool)
+    LiteralInteger int -> Int int
+    LiteralString str -> Str str
+
+type Loc = Int
+
+-- FIXME: Loc -> actual value
+data Value
+    = Unit ()
+    | Boolean Bool
+    | Int Integer
+    | Str String
+    deriving Show
+
+type Env = Map String Loc
+type Store = Map Loc Value
+
+type State = (Env, Store) -- FIXME: correct?
+initialState :: State
+initialState = (Map.empty, Map.empty)
+
+alloc :: Store -> Loc
+alloc m = if Map.null m then 0
+          else let (i, w) = Map.findMax m in i+1
+
+-- FIXME: expand stub impls
+evalStmt :: Stm -> State -> IO State
+evalStmt (SExp expr) state = do
+    print expr -- DEBUG
+    (state, value) <- (evalExp expr state)
+    print value -- DEBUG
+    return state
+
+evalExp :: Exp -> State -> IO (State, Value)
+evalExp (ELit lit) state = return (state, litToValue lit)
+
+evalExp (EDiv e1 e2) state0 = do
+    (state1, value1) <- evalExp e1 state0
+    (state2, value2) <- evalExp e2 state1
+
+    case (value1, value2) of
+        (Int int1, Int 0) -> fail "Division by 0"
+        (Int int1, Int int2) -> return (state2, Int (int1 `div` int2))
+        _ -> fail "Incompatible types"
+
+-- scopes (manages below?)
+-- variable bindings -- holds values of a given type
+-- function bindings -- holds function of a given type (no closures for now)
+
+-- expressions with side effects - evaluating expressions yields us another state
+-- evaluting from left to right
+
+
 
 
 -- type Var = String
