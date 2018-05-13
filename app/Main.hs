4479700f51c530eda12c97d45921daec45ca48cd
Igor Matuszewski <Xanewok@gmail.com>
2018-05-13 17:11:28 +0200

    Organize package layout
    * Handle parsing errors
    * Stub out interpreter

--- app/Main.hs
+++ app/Main.hs
@@ -1,66 +1,44 @@
 module Main where
 
-import Data.Function
-import Data.List
+import System.Environment(getArgs)
+import System.Exit
+import System.Directory(doesFileExist)
 
-import Lib
+import BNFC.ErrM(Err(Ok, Bad))
+import BNFC.ParStretch
+import BNFC.LexStretch
+import BNFC.AbsStretch(Program)
 
-type Var = String
+import Lib(interpret)
 
-infixl 6 :+:, :-:
-infixl 7 :*:, :/:
+lexer :: String -> [Token]
+lexer = myLexer
 
-data Exp
-    = C Int        -- constant
-    | V Var        -- variable
-    | Exp :+: Exp  -- addition
-    | Exp :-: Exp  -- subtraction
-    | Exp :*: Exp  -- multiplication
-    | Exp :/: Exp  -- division
+parser :: [Token] -> Err Program
+parser = pProgram
 
-infix 1 :=
-
-data Stmt
-    = Var := Exp      -- assignment
-    | While Exp Stmt  -- loop
-    | Seq [Stmt]      -- sequence
-
-type Prog = Stmt
-
-type Val = Int
-type Store = [(Var, Val)]
-
-eval :: Exp -> Store -> Val
-eval (C n) r       = n
-eval (V x) r       = case lookup x r of
-                       Nothing -> error ("unbound variable `" ++ x ++ "'")
-                       Just v  -> v
-eval (e1 :+: e2) r = eval e1 r + eval e2 r
-eval (e1 :-: e2) r = eval e1 r - eval e2 r
-eval (e1 :*: e2) r = eval e1 r * eval e2 r
-eval (e1 :/: e2) r = eval e1 r `div` eval e2 r
-
-exec :: Stmt -> Store -> Store
-exec (x := e) r                    = (x, eval e r) : r
-exec (While e s) r | eval e r /= 0 = exec (Seq [s, While e s]) r
-                   | otherwise     = r
-exec (Seq []) r                    = r
-exec (Seq (s : ss)) r              = exec (Seq ss) (exec s r)
-
-run :: Prog -> Store -> Store
-run p r = nubBy ((==) `on` fst) (exec p r)
-
-fib :: Prog
-fib = Seq
-  [ "x" := C 0
-  , "y" := C 1
-  , While (V "n") $ Seq
-      [ "z" := V "x" :+: V "y"
-      , "x" := V "y"
-      , "y" := V "z"
-      , "n" := V "n" :-: C 1
-      ]
-  ]
+lexAndParse :: String -> Either String Program
+lexAndParse s = case (parser . lexer) s of
+    Bad msg -> Left msg
+    Ok tree -> Right tree
 
 main :: IO ()
-main = print $ lookup "x" $ run fib [("n", 25)]
+main = do
+    args <- getArgs
+    case args of
+      (f : _) -> do
+        fileExists <- doesFileExist f
+        case fileExists of
+          False -> do
+            putStrLn $ "Error: `" ++ f ++ "`: not a file, exiting."
+            exitFailure
+          True -> do
+            contents <- readFile f
+            case lexAndParse contents of
+                Left s -> do
+                    putStrLn $ "Error: parsing failed: " ++ s
+                    exitFailure
+                Right tree -> interpret tree
+      _ -> do
+        putStrLn "Error: need a file name as argument"
+        exitFailure
